1027d368638c5c8e58e747628bee153cf0400703
nickchamp <nick.pruchkovskiy@gmail.com>
2018-01-29 18:44:58 +0200

    Point

--- src/main/java/ru/stqa/pft/java_pft/Point.java
+++ src/main/java/ru/stqa/pft/java_pft/Point.java
@@ -21,7 +21,7 @@ public class Point {
     public static void main(String[] args) {
 
       Point point1 = new Point(5, 10);
-      Point point2 = new Point(7, 14);
+      Point point2 = new Point(8, 14);
 
       System.out.println(point1.distance(point2));
     }
