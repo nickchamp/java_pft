297d49006f48eb0a02ce2f94c74fb8a5a309a111
nickchamp <nick.pruchkovskiy@gmail.com>
2018-01-29 17:56:36 +0200

    Point

---
+++ src/main/java/ru/stqa/pft/java_pft/Point.java
@@ -0,0 +1,29 @@
+package ru.stqa.pft.java_pft;
+
+/**
+ * Created by nickolay on 29.01.18.
+ */
+public class Point {
+  public double x;
+  public double y;
+
+  public Point(double x, double y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  public double distance(Point point) {
+    return Math.sqrt(Math.pow(this.x - point.x, 2) + Math.pow(this.y - point.y, 2));
+  }
+
+  public static class Distance {
+
+    public static void main(String[] args) {
+
+      Point point1 = new Point(5, 10);
+      Point point2 = new Point(7, 14);
+
+      System.out.println(point1.distance(point2));
+    }
+  }
+}
