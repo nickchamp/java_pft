3304e45f66d3fb2e43126c3ba169d3772b1f07ed
nickchamp <nick.pruchkovskiy@gmail.com>
2018-01-29 17:14:28 +0200

    Square and Rectangle Objects

---
+++ src/main/java/ru/stqa/pft/java_pft/Rectangle.java
@@ -0,0 +1,14 @@
+package ru.stqa.pft.java_pft;
+
+/**
+ * Created by nickolay on 29.01.18.
+ */
+public class Rectangle {
+  public double a;
+  public double b;
+
+  public Rectangle (double a, double b) {
+    this.a = a;
+    this.b = b;
+  }
+}
---
+++ src/main/java/ru/stqa/pft/java_pft/Square.java
@@ -0,0 +1,14 @@
+package ru.stqa.pft.java_pft;
+
+/**
+ * Created by nickolay on 29.01.18.
+ */
+public class Square {
+
+  public double l;
+
+  public Square(double l) {
+    this.l = l;
+  }
+
+}
